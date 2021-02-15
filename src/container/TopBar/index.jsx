3249e830b54bc2e1d8403e105c9942bb486f00ca
VetaZhang <veta0531@gmail.com>
2021-02-16 00:16:23 +0800

    add simple topbar

--- src/container/TopBar/index.jsx
+++ src/container/TopBar/index.jsx
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import styles from './style.less';
+
+const routerList = [
+  {
+    name: '我的仓库',
+    path: '/home'
+  },
+  {
+    name: '我的分享',
+    path: '/share'
+  }
+]
+
+function routerBtn() {
+  const history = useHistory();
+  
+  return routerList.map(router => {
+    return <div
+      className={styles.btn}
+      onClick={() => {
+        if (router.path !== history.location.pathname) {
+          history.push(router.path);
+        }
+      }}
+    >
+      {router.name}
+      <div />
+    </div>;
+  });
+}
 
 function TopBar() {
   return <div className={styles.topBar}>
-    //
+    <div className={styles.leftWrapper}>
+      {routerBtn()}
+    </div>
+    <div className={styles.rightWrapper}>
+      //
+    </div>
   </div>;
 }
 
